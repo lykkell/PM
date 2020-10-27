bafc2ec29bd1ee10ca20e2e8b76c505788587e4d
lykkell <lykkellit@gmail.com>
2020-10-27 20:14:03 +0200

    arrow-left position?!

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,8 @@
-// alert (1);
+$(document).ready(function(){
+    $('.carousel-inner').slick({
+        speed: 1200,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow_left.png"></button>',
+    });
+  });
