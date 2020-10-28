bc65c5ad9861085adf8c3aecfbaf74d255f7473b
lykkell <lykkellit@gmail.com>
2020-10-28 14:10:30 +0200

    fixed carousel-inner to carousel__inner

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,22 @@
 $(document).ready(function(){
-    $('.carousel-inner').slick({
+    $('.carousel__inner').slick({
         speed: 1200,
         slidesToShow: 1,
         // adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.png"></button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.png"></button>',
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                infinite: true,
+                dots: true,
+                arrows: false,
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
     });
   });
