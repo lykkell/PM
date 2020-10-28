ec2200b3184d98abae3e7664e7c32d819531c953
lykkell <lykkellit@gmail.com>
2020-10-28 12:45:22 +0200

    if img don`t see at PC offline
    we need change img address

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         speed: 1200,
         slidesToShow: 1,
         // adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow_left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow_right.png"></button>'
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.png"></button>'
     });
   });
