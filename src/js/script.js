4da61efa7ee48a3144489164d0dc26097e22efba
lykkell <lykkellit@gmail.com>
2020-10-28 11:54:16 +0200

    slide arrow is done

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
     $('.carousel-inner').slick({
         speed: 1200,
         slidesToShow: 1,
-        adaptiveHeight: true,
+        // adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow_left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow_right.png"></button>'
     });
   });
