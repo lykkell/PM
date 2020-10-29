5adf6b56365785eb0e2e05118afcdfc0340b6749
lykkell <lykkellit@gmail.com>
2020-10-29 21:56:27 +0200

    change slider slick to owl.carousel

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,38 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1500,
-        slidesToShow: 1,
-        // adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.png"></button>',
-        // responsive: [
-        //     {
-        //       breakpoint: 1024,
-        //       settings: {
-        //         infinite: true,
-        //         dots: true,
-        //         arrows: false,
-        //       }
-        //     }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-          ]
-    });
-  });
+// $(document).ready(function(){
+//     $('.carousel__inner').slick({
+//         speed: 1500,
+//         slidesToShow: 1,
+//         // adaptiveHeight: true,
+//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.png"></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.png"></button>',
+//         responsive: [
+//             {
+//               breakpoint: 1024,
+//               settings: {
+//                 infinite: true,
+//                 dots: true,
+//                 arrows: false,
+//               }
+//             }
+//             // You can unslick at a given breakpoint now by adding:
+//             // settings: "unslick"
+//             // instead of a settings object
+//           ]
+//     });
+//   });
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:5
+      }
+  }
+})
