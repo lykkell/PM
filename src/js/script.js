733abb29709fefb96141cc1fbe078fc8029251df
Lykkell_BLG <lykkellit@gmail.com>
2020-10-29 14:05:40 +0200

    another slider

--- src/js/script.js
+++ src/js/script.js
@@ -5,15 +5,15 @@ $(document).ready(function(){
         // adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.png"></button>',
-        responsive: [
-            {
-              breakpoint: 1024,
-              settings: {
-                infinite: true,
-                dots: true,
-                arrows: false,
-              }
-            }
+        // responsive: [
+        //     {
+        //       breakpoint: 1024,
+        //       settings: {
+        //         infinite: true,
+        //         dots: true,
+        //         arrows: false,
+        //       }
+        //     }
             // You can unslick at a given breakpoint now by adding:
             // settings: "unslick"
             // instead of a settings object
