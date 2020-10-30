00ee1f3b35ac3c40494362787fb9d460488eed95
lykkell <lykkellit@gmail.com>
2020-10-30 09:38:00 +0200

    owl slider is done
    without arrow buttons

--- src/js/script.js
+++ src/js/script.js
@@ -25,14 +25,14 @@ $('.owl-carousel').owlCarousel({
   margin:10,
   nav:true,
   responsive:{
-      0:{
-          items:1
-      },
-      600:{
-          items:3
-      },
-      1000:{
-          items:5
-      }
+    0:{
+      items:1
+    },
+    600:{
+      items:1
+    },
+    1000:{
+      items:1
+    }
   }
 })
